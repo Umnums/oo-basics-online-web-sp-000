63edfa5e56d41b4b5be7d4825571c3957d426586
Joseph Kao <josephtkao@gmail.com>
2020-04-14 02:52:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   attr_accessor :color, :size, :brand, :material, :condition
 
   def cobble
-    puts "shoe"
+    puts "Your shoe is as good as new!"
     self.condition = "new"
   end
 end
