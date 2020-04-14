5b167468b2e325e87612f823c9e7e47fa57916d4
Joseph Kao <josephtkao@gmail.com>
2020-04-14 01:39:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,4 +7,20 @@ class Book
     @title
   end
 
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def pagecount=(pagecount)
+    @pagecount = pagecount
+  end
+
+  def pagecount
+    @pagecount
+  end
+
 end
